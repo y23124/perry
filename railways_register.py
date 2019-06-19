f64094d5bdc2aee1757e7e498f7fd1bd4d98b62e
perry <perryyen0608@gamil.com>
2019-06-19 16:03:34 +0800

    add month,email,cellphone

--- railways_register.py
+++ railways_register.py
@@ -97,9 +97,9 @@ class Taiwan_railway:
             self.browser.find_element_by_xpath('/html/body').click()
 
             try:
-                WebDriverWait(self.browser, 1).until(lambda x: x.find_element_by_id("birthYear"))
-                birthYear_error = self.browser.find_element_by_id('birthYear-error').text
-                print(birthYear_error)
+                WebDriverWait(self.browser, 1).until(lambda x: x.find_element_by_id("birthYear-error"))
+                birthyear_error = self.browser.find_element_by_id('birthYear-error').text
+                print(birthyear_error)
             except:
                 print('fail')
 
@@ -121,11 +121,81 @@ class Taiwan_railway:
             print('fail')
 
 
+    def birth_month(self):
+        birthmonth = self.browser.find_element_by_id('birthMonth')
+
+        for y in m_year:
+            birthmonth.send_keys(y)
+            self.browser.find_element_by_xpath('/html/body').click()
+
+            try:
+                WebDriverWait(self.browser, 1).until(lambda x: x.find_element_by_id("birthMonth-error"))
+                birthmonth_error = self.browser.find_element_by_id('birthMonth-error').text
+                print(birthmonth_error)
+            except:
+                print('fail')
+
+            time.sleep(1)
+            birthmonth.clear()
+
+
+    def month_btn(self):
+        datepickermonth = self.browser.find_element_by_xpath('//*[@id="myForm"]/div[6]/div/div[2]/div/button')
+        datepickermonth.click()
+
+        try:
+            WebDriverWait(self.browser, 1).until(lambda x: x.find_element_by_xpath('/html/body/div[5]'))
+            choice_month = self.browser.find_element_by_xpath('/html/body/div[5]/div[2]/table/tbody/tr/td/span[1]')
+            choice_month.click()
+        except:
+            print('fail')
+
+
+    def email_error(self):
+        register_email = self.browser.find_element_by_id('email')
+
+        for e in email:
+            register_email.send_keys(e)
+            self.browser.find_element_by_xpath('/html/body').click()
+
+            try:
+                WebDriverWait(self.browser, 1).until(lambda x: x.find_element_by_id("email-error"))
+                email_error = self.browser.find_element_by_id('email-error').text
+                print(email_error)
+            except:
+                print('fail')
+
+            time.sleep(1)
+            register_email.clear()
+
+
+    def cellphone_error(self):
+        register_cellphon = self.browser.find_element_by_id('cellphoneNo')
+
+        for c in cellphone:
+            register_cellphon.send_keys(c)
+            self.browser.find_element_by_xpath('/html/body').click()
+
+            try:
+                WebDriverWait(self.browser, 1).until(lambda x: x.find_element_by_id("cellphoneNo-error"))
+                cellphone_error = self.browser.find_element_by_id('cellphoneNo-error').text
+                print(cellphone_error)
+            except:
+                print('fail')
+
+            time.sleep(1)
+            register_cellphon.clear()
+
+
+
 if __name__ == "__main__":
     chromedriver_path = r'C:\Users\ADMIN\PycharmProjects\chromedriver.exe'
     url = r'https://www.railway.gov.tw/tra-tip-web/tip'
     pid_number = '123'
     password = ['aaaaaaaa','12345678','aa1234','aaaa123456789']
     b_year = ['test','!@#','測試',1899,2100,2010]
+    m_year = [0,5,00,13,'test','測試']
+    email = ['123','123@','123@測試','123@#$%','123@test']
+    cellphone = ['123']
     a = Taiwan_railway()
-    a.year_btn()
+    a.cellphone_error()
